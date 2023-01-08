7293c3f2036b9757a881b0e8df26951ba79ee62c
Ken-dono <63581309+Ken-dono@users.noreply.github.com>
2023-01-08 19:05:46 +0100

    Add files via upload

---
+++ script-insertion-utilisateur.sql
@@ -0,0 +1,120 @@
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (1,"Kristen","Cline",84,"3985 Quam Rd.","UIN23TJZ9EK",8),
+  (2,"Jamalia","Merritt",38,"Ap #472-1302 Integer St.","TOE65JRH3WI",8),
+  (3,"Clark","Ayala",79,"P.O. Box 934, 7751 Risus. Road","RBB23YBE8VN",13),
+  (4,"Kennan","Jarvis",55,"734-6639 Vivamus Avenue","DXK65FJM2US",5),
+  (5,"Jeremy","Warner",64,"393-6087 Neque. Road","TID98QRK5NB",6),
+  (6,"Venus","Heath",57,"P.O. Box 342, 4081 Cras Av.","STT68JIU7DH",10),
+  (7,"Kenneth","Kent",36,"Ap #202-6856 Enim, Rd.","WUB73QNH7KJ",14),
+  (8,"Tashya","Malone",88,"2915 Fringilla Road","KFY66EVJ8RJ",18),
+  (9,"Chantale","Newton",83,"Ap #956-8312 Ornare Av.","QVH04OOG2PJ",15),
+  (10,"Bert","Burns",77,"Ap #646-3987 Ornare, Rd.","OJG41AFO8MO",9);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (11,"Jamal","Briggs",78,"P.O. Box 782, 6744 Metus Rd.","JUC51LTW4SY",26),
+  (12,"Tanner","Sosa",44,"125-4572 In Rd.","LWF51XTC7AF",14),
+  (13,"Yoshio","Carroll",67,"973-1777 Purus Road","GQQ82UQR0BL",22),
+  (14,"Sara","Blanchard",47,"P.O. Box 832, 4165 Eu, Avenue","RKP58PDU8JW",9),
+  (15,"Adara","Fuentes",81,"5991 Ultrices Avenue","SFE47EKP9IY",13),
+  (16,"Armand","Heath",26,"465-6261 Magna. Avenue","EBY69VKX5RP",20),
+  (17,"Xena","Bender",43,"480-3133 Id Street","RHU77YKT8MP",21),
+  (18,"Vladimir","Cunningham",84,"P.O. Box 679, 9291 Pharetra St.","EXK26SVD5YS",5),
+  (19,"Savannah","Pearson",41,"P.O. Box 729, 5818 Massa. Rd.","KNR81PRC3UG",24),
+  (20,"Piper","Head",34,"9188 Parturient Rd.","VLH67VQN7IO",14);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (21,"Demetria","Vang",33,"185 Nibh Street","LQL02UAG0YP",4),
+  (22,"Bradley","Beck",40,"P.O. Box 749, 6666 Integer St.","DIM52ACS3KL",17),
+  (23,"Jane","Dodson",22,"2869 Orci St.","FFT63YLE8YX",13),
+  (24,"David","Luna",72,"2466 Lorem. Rd.","DTR73BFM4WW",14),
+  (25,"Nadine","Vasquez",51,"2671 Placerat St.","NGV75EBT8MR",4),
+  (26,"Emily","Kim",69,"Ap #468-4149 Orci Road","IHY64VJK1JB",15),
+  (27,"Lisandra","French",31,"724-4490 Metus. Road","QUG24EQQ0FS",5),
+  (28,"McKenzie","Stevenson",46,"Ap #665-5748 Iaculis Road","EXM78NIK3XY",15),
+  (29,"Demetrius","Boyle",63,"375-2360 Amet, Street","GCL66RCW4KJ",1),
+  (30,"Quinn","Vaughan",37,"335-8545 In, St.","PCF58QDU2TJ",8);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (31,"Charles","Johnston",53,"9152 Scelerisque Road","DLU15XGG5QS",7),
+  (32,"Aphrodite","Ayala",52,"Ap #781-7696 Justo. Rd.","OLW23BYL1ZK",9),
+  (33,"Karly","Rose",44,"Ap #143-8864 Phasellus St.","FNQ74JLS2JF",18),
+  (34,"Skyler","Wong",62,"Ap #513-4181 Aenean St.","VQK14HAS5QF",17),
+  (35,"Kylee","Christensen",64,"Ap #412-2718 Justo. Rd.","EYI85CWZ6XM",19),
+  (36,"Kim","Chase",40,"Ap #499-5271 Convallis Street","QJA67CGF3MO",16),
+  (37,"Adrian","Clements",45,"6179 Ornare, Rd.","RUY44FAY0DX",6),
+  (38,"Alexander","Bush",27,"Ap #619-7168 Aliquam St.","WRS93QCB7LG",6),
+  (39,"Trevor","Nicholson",29,"Ap #509-3074 Et Rd.","CEJ87RRM7HB",20),
+  (40,"Charity","Valenzuela",37,"546 Metus. Ave","UQJ36WCT8GO",2);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (41,"Sarah","Pugh",23,"7369 Semper Avenue","DDN11VIG5SH",7),
+  (42,"Marvin","Riddle",44,"Ap #732-1864 Scelerisque St.","OCP82KJN6ZN",2),
+  (43,"Francesca","Coleman",70,"355-6270 Felis Ave","SSB45CZO8IA",15),
+  (44,"Gray","Benjamin",82,"567-4185 Aliquam St.","MKI19XOL2HG",3),
+  (45,"Tatiana","Coleman",54,"Ap #406-9116 Leo. Rd.","GUC27DGR1BP",3),
+  (46,"Hermione","Harper",36,"Ap #837-4390 Eu Road","MPD04AOA3EV",16),
+  (47,"Walker","Howard",68,"P.O. Box 163, 8359 Quam. St.","SKM80LJH8IM",7),
+  (48,"Denise","Salazar",51,"Ap #686-997 Arcu. St.","ERY81WAA8TI",6),
+  (49,"Katell","Morton",26,"Ap #478-4791 Enim. Ave","TBW34TQV8IO",9),
+  (50,"Hadley","Sellers",50,"Ap #370-4324 Quisque St.","HJL79HOY7YJ",11);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (51,"Kato","Walters",36,"Ap #118-1783 Sapien Rd.","TCG37NHG1QV",12),
+  (52,"Inez","Vega",70,"P.O. Box 882, 3187 Et Ave","IBC78BLN3JW",19),
+  (53,"Brianna","Cherry",68,"P.O. Box 269, 3791 Dui. Av.","LLY14XHO9CJ",7),
+  (54,"Jackson","Nielsen",32,"P.O. Box 368, 3134 Nec Rd.","HOG18IFR1BT",16),
+  (55,"Palmer","Kaufman",73,"6006 Tristique Rd.","EWM86NVW2MU",24),
+  (56,"Kasimir","Pratt",53,"778-5301 Tempor Street","DKB53EIW5PV",20),
+  (57,"Amanda","Huber",46,"180-5098 Condimentum Road","KBV50BWR6NP",2),
+  (58,"Kuame","Mcdowell",24,"Ap #130-8007 Donec Ave","VBW13MCQ0GH",10),
+  (59,"Shafira","Newman",53,"P.O. Box 636, 2156 Sed Avenue","VFJ63RIS9HE",12),
+  (60,"Burton","Olsen",17,"P.O. Box 941, 5069 Amet St.","RFL32GKQ0WH",17);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (61,"Abdul","Ballard",72,"P.O. Box 489, 9512 Quis Rd.","LFR37HOB3JS",15),
+  (62,"Gary","Oneil",83,"Ap #370-8550 Erat Street","KJD13LBQ7IH",24),
+  (63,"Moses","Harris",79,"Ap #261-8593 Ornare Avenue","EMR28PWS0AH",20),
+  (64,"Sage","Rice",49,"Ap #839-9891 Interdum. Av.","PXS63BPY5UK",2),
+  (65,"Norman","Garcia",89,"P.O. Box 914, 9046 Nibh Av.","KTO89FOK6WM",13),
+  (66,"Germane","Todd",84,"163-6187 In Street","XGW15MNV7GI",14),
+  (67,"Stacy","Casey",80,"814-6305 Pharetra. St.","SLR10BAQ4DF",18),
+  (68,"September","Henry",80,"6388 Sapien Av.","YWW65NCC5SN",15),
+  (69,"Kaye","Gould",59,"103-1019 Eget Street","HRD47NOT7XR",4),
+  (70,"Chiquita","Copeland",54,"8916 Ac Avenue","THT26JMM9NB",25);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (71,"Judah","Talley",73,"365-5756 Sed Rd.","ZLT15ECY3EH",27),
+  (72,"Cassidy","Richard",82,"448-6622 Aliquam, Av.","GXB09XHO2EJ",1),
+  (73,"Jesse","Maynard",16,"Ap #280-6900 Velit St.","OFZ96MGX2KR",6),
+  (74,"Sonia","Pace",46,"Ap #202-8929 Duis Avenue","BHP88TJH7EM",19),
+  (75,"Timon","Lowery",58,"6299 Risus Ave","BJV53KDW4IQ",6),
+  (76,"Mohammad","William",30,"P.O. Box 851, 4060 Pharetra Avenue","NFM60AFS2AY",0),
+  (77,"Robin","Meyer",64,"5014 A, Avenue","JTT23JRR8BJ",19),
+  (78,"Amber","Nunez",26,"Ap #391-992 Dolor, Street","GHH92DKB2SX",21),
+  (79,"Janna","Hill",63,"P.O. Box 522, 5764 Feugiat. Ave","CCK19GBC8EI",24),
+  (80,"Idona","Mccarthy",79,"588-4075 Enim Rd.","UEO82JCM3AF",25);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (81,"Tasha","Clements",42,"572-9378 Molestie Rd.","IUH33SBI4DI",4),
+  (82,"Drew","George",81,"P.O. Box 948, 4424 Cras Road","IIW93FBS1TO",15),
+  (83,"Elmo","Emerson",32,"Ap #279-8604 Eu St.","OEX23RPN4SC",12),
+  (84,"Nathaniel","Dixon",49,"Ap #608-1228 Massa. Av.","YHW91WRG4AA",12),
+  (85,"Jasper","Parsons",40,"7605 Blandit Street","KVY36REG4IK",14),
+  (86,"Yoshi","Larson",16,"Ap #814-2535 Nam Rd.","FMX63FHC2HE",21),
+  (87,"Eric","Morse",45,"P.O. Box 445, 2019 Elit. Av.","RLU20RQJ8LR",8),
+  (88,"Hall","Oneil",60,"Ap #790-616 Laoreet Rd.","IEX46BPA4YU",11),
+  (89,"Igor","Raymond",62,"2023 Neque. Rd.","SDD54PTB2JZ",6),
+  (90,"Susan","Mcbride",32,"Ap #870-9969 Lacinia Street","UMD81FED5OQ",16);
+INSERT INTO Utilisateur (numUtilisateur,nom,prenom,age,adresse,motDePasse,numClub)
+VALUES
+  (91,"Ryder","Contreras",89,"1693 Class St.","AEJ06BHM9CB",2),
+  (92,"Emily","Daugherty",57,"Ap #853-2193 Est. Ave","MJS63UUX5EF",0),
+  (93,"Christian","Walker",19,"335-7870 Tellus Avenue","YWJ09YBI4GI",8),
+  (94,"Hedda","Huber",75,"Ap #229-7708 Gravida Street","XSJ26KBB6JC",25),
+  (95,"Portia","Battle",53,"Ap #339-9152 A, Av.","ITH55RPC4CD",20),
+  (96,"Sopoline","Dyer",18,"247-5101 Commodo Street","RPD61XCD3PL",18),
+  (97,"Valentine","Fuentes",62,"549 Quis, Avenue","BWV82DHO4XW",19),
+  (98,"Celeste","Shepard",80,"7826 Nisi Road","RFS48FKS4JO",17),
+  (99,"Fuller","Burgess",48,"690-2136 Accumsan St.","FYV38WTD2AG",9),
+  (100,"Alisa","Poole",74,"605-9316 Non Av.","KGC68QSQ7CS",7);
